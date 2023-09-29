89a6feb5ea1b970629d0b5e4f48f7978324a2897
XLianLZ <Lianbox7304@gmail,com>
2023-09-28 22:41:27 -0500

    inicio sesion ts

--- src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,42 @@
+// inicio-sesion.component.ts
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';  // Asegúrate de importar desde el camino correcto
+
+@Component({
+  selector: 'app-inicio-sesion',
+  templateUrl: './inicio-sesion.component.html',
+  styleUrls: ['./inicio-sesion.component.css']
+})
+export class InicioSesionComponent implements OnInit {
+  
+  loginForm!: FormGroup;
+  
+  constructor(private fb: FormBuilder, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.userService.getUserByEmail(this.loginForm.value.correo).subscribe(user => {
+        if (user && user.contrasena === this.loginForm.value.contrasena) {
+          console.log('Inicio de sesión exitoso');
+        } else {
+          console.log('Inicio de sesión fallido');
+        }
+      });
+    }
+  }
+}
+
+
+/*import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -23,3 +61,4 @@ export class InicioSesionComponent implements OnInit {
     }
   }
 }
+*/
