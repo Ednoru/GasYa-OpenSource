9ce739f704778d75855e8f127587bec013180672
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-01 17:16:17 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GasYa';
+  isLoggedIn: boolean = false;
   options = [
     { path: '/buscarGrifo', title: 'Buscar Grifo' },
     { path: '/perfil', title: 'Perfil' },
@@ -16,4 +17,11 @@ export class AppComponent {
     { path: '/iniciosesion', title: 'Iniciar Sesion' },
     { path: '/registro', title: 'Registro' },
   ];
+
+  cerrarSesion() {
+    // Agrega la lógica necesaria para cerrar la sesión aquí
+    // Por ejemplo, puedes realizar una llamada a un servicio de autenticación
+    // y cambiar el valor de 'isLoggedIn' a 'false'.
+    this.isLoggedIn = false;
+  }
 }
