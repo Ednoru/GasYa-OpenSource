7624563674a8970c64cd83dc00502f404780ac0d
Eduardo Chero <eduardochero08@gmail.com>
2023-11-04 01:46:51 -0500

    busqueda de grifos

--- src/app/components/buscar-grifo/buscar-grifo.component.ts
+++ src/app/components/buscar-grifo/buscar-grifo.component.ts
@@ -2,7 +2,13 @@ import { Component, AfterViewInit } from '@angular/core';
 
 declare namespace google.maps {
   export class Map {
+    private map: any;
     constructor(mapDiv: Element, opts?: MapOptions);
+
+    // @ts-ignore
+    getCenter(): google.maps.LatLng {
+      return this.map.getCenter() as google.maps.LatLng;
+    }
   }
 
   export interface MapOptions {
@@ -19,6 +25,7 @@ declare namespace google.maps {
     map?: Map;
     position?: LatLng | LatLngLiteral;
     title?: string;
+    icon?: string;
   }
 
   export interface LatLng {
@@ -29,6 +36,16 @@ declare namespace google.maps {
   export interface LatLngLiteral {
     lat: number;
     lng: number;
+  }
+
+  export class places {
+    static PlacesServiceStatus: any;
+    constructor(mapDiv: Element, opts?: MapOptions);
+    nearbySearch(request: any, callback: any): void;
+  }
+
+  export class PlacesService {
+    constructor(mapDiv: Element, opts?: MapOptions);
   }
 }
 
@@ -46,6 +63,9 @@ declare global {
 })
 export class BuscarGrifoComponent implements AfterViewInit {
   map: google.maps.Map;
+  // @ts-ignore
+  placesServices: google.maps.places.PlacesService;
+  searchQuery: string = ''
 
   constructor() {
     this.map = {} as google.maps.Map;
@@ -65,15 +85,42 @@ export class BuscarGrifoComponent implements AfterViewInit {
         mapId: 'DEMO_MAP_ID',
       });
 
+      this.placesServices = new googleMaps.places.PlacesService(this.map);
+      /*
       new googleMaps.Marker({
         map: this.map,
         position: position,
         title: 'Uluru'
       });
+
+      this.searchNearbyGasStation(position);
+      */
+
     }).catch((error: any) => {
       console.error(error);
     });
   }
+
+  searchNearbyGasStation(): void {
+    const request = {
+      location: this.map.getCenter(),
+      radius: '5000',
+      type: ['gas_station']
+    };
+
+    this.placesServices.nearbySearch(request, (results: any, status: any) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (const place of results) {
+          new google.maps.Marker({
+            map: this.map,
+            position: place.geometry.location,
+            title: place.name,
+          });
+        }
+      }
+    });
+  }
+
 
   loadGoogleMaps(): Promise<any> {
     return new Promise((resolve, reject) => {
@@ -89,7 +136,7 @@ export class BuscarGrifoComponent implements AfterViewInit {
         };
 
         const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDmVzKjNKXtezL0qdB8Os4tIFJnS0kOAUA`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDmVzKjNKXtezL0qdB8Os4tIFJnS0kOAUA&libraries=places`;
         script.async = true;
         script.defer = true;
         script.onload = () => {
