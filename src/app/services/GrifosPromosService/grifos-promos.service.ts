201fc3a9b8d698a128addb5d06d6ca1943bac7cd
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-21 19:22:43 -0500

    coenxion a la api 100% real no feik

--- src/app/services/GrifosPromosService/grifos-promos.service.ts
+++ src/app/services/GrifosPromosService/grifos-promos.service.ts
@@ -5,8 +5,11 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class GrifosPromosService {
-  private grifos_api = 'https://my-json-server.typicode.com/Ednoru/GasYa-OpenSource/grifos';
-  private promos_api = 'https://my-json-server.typicode.com/Ednoru/GasYa-OpenSource/promos';
+  //private grifos_api_fake_img = 'https://my-json-server.typicode.com/Ednoru/GasYa-OpenSource/grifos';
+  private grifos_api = 'http://localhost:8080/api/gasStations'
+  //private promos_api_fake_img = 'https://my-json-server.typicode.com/Ednoru/GasYa-OpenSource/promos';
+  private promos_api = 'http://localhost:8080/api/promotions'
+  private combustible_api = 'http://localhost:8080/api/fuels'
 
   constructor(private http: HttpClient) { }
 
