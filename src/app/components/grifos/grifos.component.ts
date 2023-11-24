644556a5c864a35d739dc4d31bee0178804ce506
Eduardo Chero <eduardochero08@gmail.com>
2023-11-23 21:51:31 -0500

    Implementacion de la API

--- src/app/components/grifos/grifos.component.ts
+++ src/app/components/grifos/grifos.component.ts
@@ -1,83 +1,66 @@
-/*
-import {Component} from '@angular/core';
-
-
-@Component({
-  selector: 'app-grifos',
-  templateUrl: './grifos.component.html',
-  styleUrls: ['./grifos.component.css']
-})
-export class GrifosComponent {
-  botonActivo: string | null = null;
-
-  mostrar(idDiv: string): void {
-    this.botonActivo = idDiv;
-  }
-}
-*/
-
-import { Component, OnInit } from '@angular/core';
-import {GrifosPromosService} from "../../services/GrifosPromosService/grifos-promos.service";
+import {Component, OnInit} from "@angular/core";
+import {
+  Fuels,
+  GasStation,
+  GrifosPromosService,
+  Promotion
+} from "../../services/GrifosPromosService/grifos-promos.service";
 
 @Component({
   selector: 'app-grifos',
   templateUrl: './grifos.component.html',
   styleUrls: ['./grifos.component.css']
 })
-export class GrifosComponent implements OnInit {
+
+export class GrifosComponent implements OnInit{
   botonActivo: string | null = null;
-  grifos: any[] = [];
-  promociones: any[] = [];
+  grifos: GasStation[] = [];
+  promociones: Promotion[] = [];
+  combustibles: Fuels[] = [];
 
-  constructor(private grifosService: GrifosPromosService) { }
+  constructor(private grifosService: GrifosPromosService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.loadGrifos();
     this.loadPromociones();
   }
 
-  async loadGrifos(): Promise<void> {
+  async loadGrifos() {
     try {
-      this.grifosService.getGrifos().subscribe((data: any) => {
-        if (Array.isArray(data)) {
-          this.grifos = data;
-        } else {
-          console.error("El servicio no devolvió una matriz.");
-        }
+      this.grifosService.getGrifos().subscribe((data: GasStation[]) => {
+        this.grifos = data;
       });
-    } catch (error) {
-      console.error(error);
+    }catch (error) {
+      console.log(error);
     }
   }
 
   async loadPromociones(): Promise<void> {
     try {
-      this.grifosService.getPromos().subscribe((data: any) => {
-        if (Array.isArray(data)) {
-          this.promociones = data;
-        } else {
-          console.error("El servicio no devolvió una matriz.");
-        }
+      this.grifosService.getPromociones().subscribe((data: Promotion[]) => {
+        this.promociones = data;
       });
     } catch (error) {
       console.error(error);
     }
   }
 
-  mostrar(nombreGrifo: string): void {
-    this.botonActivo = nombreGrifo;
-    const todosLosDivs = [
-      'Repsol',
-      'PetroPeru',
-      'Primax',
-      'Pecsa'
-    ];
-    todosLosDivs.forEach(nombre => {
-      const div = document.getElementById(nombre);
-      if (div) {
-        div.style.display = nombre === nombreGrifo ? "block" : "none";
-      }
-    });
+  async loadCombustiblesPorGrifo(grifoId: number): Promise<void> {
+    try {
+      this.grifosService.getCombustiblesPorGrifo(grifoId).subscribe((data: Fuels[]) => {
+        this.combustibles = data;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async mostrar(nombreGrifo: string, grifoId: number): Promise<void> {
+    try {
+      await this.loadCombustiblesPorGrifo(grifoId);
+      this.botonActivo = nombreGrifo;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
-
--- src/app/services/GrifosPromosService/grifos-promos.service.ts
+++ src/app/services/GrifosPromosService/grifos-promos.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {catchError, Observable} from "rxjs";
+
+
+export interface GasStation {
+  id: number;
+  name: string;
+}
+
+export interface Promotion {
+  id: number;
+  title: string;
+  description: string;
+}
+
+export interface Fuels {
+  id: number;
+  type: string;
+  description: string;
+  price: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GrifosPromosService {
-  private grifos_api = 'http://localhost:8080/api/gasStations';
-  private promos_api = 'http://localhost:8080/api/promotions';
-  private combustible_api = 'http://localhost:8080/api/fuels';
+  private apiUrl = 'http://localhost:8080/api';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getGrifos(): Observable<any[]> {
-    return this.http.get<any[]>(this.grifos_api);
+  getGrifos(): Observable<GasStation[]> {
+    return this.http.get<GasStation[]>(`${this.apiUrl}/gasStations`).pipe(catchError(this.handleError));
   }
 
-  getPromos(): Observable<any[]> {
-    return this.http.get<any[]>(this.promos_api);
+  getPromociones(): Observable<Promotion[]> {
+    return this.http.get<Promotion[]>(`${this.apiUrl}/promotions`).pipe(
+      catchError(this.handleError)
+    );
   }
 
-  getCombustible(): Observable<any[]> {
-    return this.http.get<any[]>(this.combustible_api);
+  async getCombustiblesPorGrifo(grifoId: number): Promise<Fuels[]> {
+    try {
+      const data = await this.http.get<Fuels[]>(`${this.apiUrl}/fuels?gasStationId=${grifoId}`).toPromise();
+      return data || []; // Si data es undefined, devolvemos un array vacío
+    } catch (error) {
+      console.error('Error fetching fuels', error);
+      throw error;
+    }
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.error('An error occurred', error);
+    return error.message || error;
   }
 }
